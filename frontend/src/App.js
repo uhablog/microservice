13f7d4896a86ef54eb84bc3a3fc5c0e263699e83
uhablog <uha.blog98@gmail.com>
2024-09-19 16:43:21 +0900

    フロントエンドからBFFを叩く

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const LIST_BOOKS = gql`
+  query ListBooks {
+    books {
+      id
+      title
+      author
+      price
+    }
+  }
+`;
+
+function App() {
+
+  const { loading, error, data } = useQuery(LIST_BOOKS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>書籍名</th>
+          <th>著者</th>
+          <th>価格&nbsp;(円)</th>
+        </tr>
+        {data.books.map((row) => (
+          <tr>
+            <td>{row.title}</td>
+            <td>{row.author}</td>
+            <td align="right">{row.price}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default App;
